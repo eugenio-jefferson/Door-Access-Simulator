392fc8594679008107789c04e545c43125f1eabd
Eugênio Jefferson <eugeniojeffersonsantana@gmail.com>
2026-07-10 10:09:32 -0300

    feat: card read event

---
+++ src/main/java/com/example/model/CardReadEvent.java
@@ -0,0 +1,43 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+public class CardReadEvent {
+    private String userId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private AccessDirection direction;
+
+    public CardReadEvent(String userId, LocalDateTime timestamp, AccessDirection direction) {
+        this.userId = userId;
+        this.timestamp = timestamp;
+        this.direction = direction;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public AccessDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(AccessDirection direction) {
+        this.direction = direction;
+    }
+}
